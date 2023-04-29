01251f3d35b2d4bedc5d4650e501873f8cf38d98
alpendop <nguybi0@gmail.com>
2023-04-29 19:18:02 +0100

    Re-work the delete button
    Before this change, logging out would not remove user from localstorage, which causes the buttons to still appear in the home view

--- resources/webui/src/components/PostCard.vue
+++ resources/webui/src/components/PostCard.vue
@@ -15,8 +15,8 @@ const authStore = useAuthStore();
     <li>
       <p>{{ posts.name }}</p>
       <p>{{ posts.post }}</p>
-      <div v-if="authStore.authUser.id == posts.user_id">
-        <button @click="postStore.deletePost(posts.id)">X</button>
+      <div v-if="authStore.authUser">
+        <button @click="postStore.deletePost(posts.id)" v-show="authStore.authUser.id == posts.user_id">X</button>
       </div>
     </li>
   </ul>
