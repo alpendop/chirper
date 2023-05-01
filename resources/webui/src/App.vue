61336a3401efb57871f3d7dec542fa52e37a56f1
alpendop <nguybi0@gmail.com>
2023-05-01 21:09:36 +0100

    Move navigation header to a separate component

--- resources/webui/src/App.vue
+++ resources/webui/src/App.vue
@@ -1,25 +1,13 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
 import { useAuthStore } from './stores/auth.js';
+import Header from './components/Header.vue';
 
 const authStore = useAuthStore();
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <template v-if="!authStore.authUser">
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/register">Register</RouterLink>
-      </template>
-      <template v-else>
-        <button @click="authStore.logout()">Logout</button>
-      </template>
-    </nav>
-  </header>
-
+  <Header />
   <RouterView />
 </template>
 
---
+++ resources/webui/src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useAuthStore } from '../stores/auth.js';
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <template v-if="!authStore.authUser">
+        <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/register">Register</RouterLink>
+      </template>
+      <template v-else>
+        <button @click="authStore.logout()">Logout</button>
+      </template>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
